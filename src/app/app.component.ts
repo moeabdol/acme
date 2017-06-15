0917dce6a04e2d351dc9580e2c5f5d957dc13427
moeabdol <admin.r99@gmail.com>
2017-06-15 23:04:49 +0300

    Section 8 - Services & Dependency Injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service';
+
 @Component({
   selector: 'pm-app',
   moduleId: module.id,
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
